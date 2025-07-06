0a68423c2cd85608a2594632e59334ddf45b0d76
pearseb <pjb581@gadi-login-07.gadi.nci.org.au>
2025-07-06 11:39:55 +1000

    Converted all files from CRLF (windows-based text) to LF (unix-based text)

--- lite/setup.py
+++ lite/setup.py
@@ -1,1 +1,14 @@
-from setuptools import setup, find_packagessetup(    name="pyWOMBAT-lite",    version="0.1",    packages=find_packages(),    install_requires=[        "numpy",        "numba",    ],    description="A 1D biogeochemical model package of WOMBAT-lite",    author="Pearse James Buchanan",    license="CSIRO",)
+from setuptools import setup, find_packages
+
+setup(
+    name="pyWOMBAT-lite",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "numba",
+    ],
+    description="A 1D biogeochemical model package of WOMBAT-lite",
+    author="Pearse James Buchanan",
+    license="CSIRO",
+)
